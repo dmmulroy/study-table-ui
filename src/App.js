01f5a93f7457c911e2ccc779d92c063f8da756e1
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-08-12 21:46:13 -0400

    Change title, add Font Awesome CDN.
    Build initial login skeleton

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { PureComponent } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      message: ''
-    };
-  }
-  async componentDidMount() {
-    const { data } = await axios.get('http://localhost:3001/hello-world');
-    const { message = '' } = data;
-    this.setState(() => ({ message }));
-  }
-
+class App extends PureComponent {
   render() {
     return (
       <section className="section">
         <div className="container">
-          <h1 className="title">
-            {this.state.message}
-          </h1>
-          <p className="subtitle">
-            My first website with <strong>Bulma</strong>!
-          </p>
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter has-text-centered">
+              <h1 className="title">Study Table</h1>
+              <h2 className="subtitle">A tool for tracking study hours</h2>
+              <span className="icon is-large">
+                <i className="fa fa-book" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control has-icons-left">
+                  <input className="input is-large" type="email" />
+                  <span className="icon is-left">
+                    <i className="fa fa-envelope" />
+                  </span>
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control has-icons-left">
+                  <input className="input is-large" type="password" />
+                  <span className="icon is-left">
+                    <i className="fa fa-lock" />
+                  </span>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control">
+                  <a
+                    href="http://localhost:3000"
+                    className="button is-medium is-primary"
+                  >
+                    Login
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
