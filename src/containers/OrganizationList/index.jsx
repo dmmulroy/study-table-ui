395aa1cbdc573bca808fa7126f13d48bcb0ba5de
Dillon Mulroy <dillon.mulroy@gmail.com>
2017-10-08 18:11:41 -0400

    Initial org list styling

--- src/containers/OrganizationList/index.jsx
+++ src/containers/OrganizationList/index.jsx
@@ -14,19 +14,53 @@ class OrganizationList extends Component {
     const { data } = organizations;
 
     return (
-      <ul>
-        {Object.values(data).map(organization => (
-          <li key={organization.id}>{organization.name}</li>
-        ))}
-      </ul>
+      <div className="box content">
+        <h1>Your Organizations</h1>
+        <ul>
+          {Object.values(data).map(organization => (
+            <li
+              key={organization.id}
+              className="box"
+              style={{
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              <span
+                style={{
+                  fontSize: '3em',
+                  padding: '5px',
+                  borderRadius: '5px',
+                  boxShadow:
+                    '0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1)',
+                  display: 'inline-block',
+                  minWidth: '85px',
+                  textAlign: 'center',
+                  lineHeight: '1em',
+                  marginRight: '5px'
+                }}
+              >
+                {organization.name.charAt(0)}
+              </span>
+              <p
+                style={{
+                  display: 'flex',
+                  flex: '1',
+                  marginLeft: '5px',
+                  fontSize: '1.5em'
+                }}
+              >
+                {organization.name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
-const mapStateToProps = ({ organizations, user }) => ({
-  organizations,
-  user
-});
+const mapStateToProps = ({ organizations, user }) => ({ organizations, user });
 
 export default withRouter(
   connect(mapStateToProps, { requestUserOrganizations })(OrganizationList)
